7cb07c37527f9c47265769956489d76f0f8f28ab
Mixail89 <Mixail@m>
2023-02-15 11:11:27 +0300

    Попытка02

---
+++ C#numbers_004/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < a)
+{
+    if (i % 2 == 0)
+    {
+
+        Console.WriteLine(i + "");
+    }
+
+    i++;
+    //Console.WriteLine("Все четные числа");
+    //Console.Write(i);
+}
+Console.WriteLine("Все четные числа");
+Console.Write(i);
